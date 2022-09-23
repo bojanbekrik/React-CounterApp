479b701e6624cc1e5f374e335946254989ef12c4
Bojan Bekrikj <bojanbekrik@gmail.com>
2022-09-23 09:43:04 +0200

    //

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// first way
+const App = () => {
+    const [count, setCount] = useState(0) //count = currentStateValue , setCount = function
+
+    useEffect(() => {
+        document.title = `Clicked ${count} times`;
+    })
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <h2>counter app</h2>
+            <button onClick={increment}>Clicked {count} times</button>
+        </div>
+    );
 }
 
 export default App;
+
+// second way
+// class App extends Component {
+//     state = {
+//         count: 0
+//     }
+//
+//     increment = () => {
+//         this.setState({
+//             count: this.state.count + 1
+//         });
+//     };
+//
+//     componentDidMount() {
+//         document.title = `Clicked ${this.state.count} times`
+//     }
+//
+//     componentDidUpdate() {
+//         document.title = `Clicked ${this.state.count} times`
+//     }
+//
+//     render() {
+//         return (
+//           <div>
+//               <h2>counter app</h2>
+//               <button onClick={this.increment}>Clicked {this.state.count} times</button>
+//           </div>
+//         );
+//
+//     }
+// }
